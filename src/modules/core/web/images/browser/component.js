f2e70c3138a1f09e66c7353d5b6ed18a518e4f6d
Michael Matveev <xtreme@rh1.ru>
2020-09-10 15:54:54 +0300

    Core/Image Browser WIP

---
+++ src/modules/core/web/images/browser/component.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-webpack-loader-syntax */
+module.exports = class {
+    onCreate() {
+        const state = {};
+        this.state = state;
+    }
+};
---
+++ src/modules/core/web/images/index.js
@@ -0,0 +1,32 @@
+import Auth from "../../../../shared/lib/auth";
+import template from "./template.marko";
+import C from "../../../../shared/lib/constants";
+
+export default () => ({
+    async handler(req, rep) {
+        const auth = new Auth(this.mongo.db, this, req, rep, C.USE_COOKIE_FOR_TOKEN);
+        try {
+            const site = new req.ZoiaSite(req, "core");
+            if (!(await auth.getUserData()) || !auth.checkStatus("admin")) {
+                auth.clearAuthCookie();
+                return rep.redirectToLogin(req, rep, site, req.zoiaConfig.routes.imagesBrowser);
+            }
+            site.setAuth(auth);
+            const render = await template.stream({
+                $global: {
+                    serializedGlobals: {
+                        template: true,
+                        pageTitle: true,
+                        ...site.getSerializedGlobals()
+                    },
+                    template: "admin",
+                    pageTitle: site.i18n.t("moduleTitle"),
+                    ...site.getGlobals(),
+                }
+            });
+            return rep.sendHTML(rep, render);
+        } catch (e) {
+            return Promise.reject(e);
+        }
+    }
+});
