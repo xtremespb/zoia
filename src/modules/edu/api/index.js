808334168dfaaf063059c229d0671ef5a77bed05
Michael Matveev <xtreme@rh1.ru>
2020-11-03 10:49:16 +0300

    Removed EDU module from master

--- src/modules/edu/api/index.js
+++
@@ -1,9 +0,0 @@
-import apiQuestions from "./apiQuestions";
-import apiAnswers from "./apiAnswers";
-import apiFinish from "./apiFinish";
-
-export default fastify => {
-    fastify.post("/api/edu/questions", apiQuestions());
-    fastify.post("/api/edu/answers", apiAnswers());
-    fastify.post("/api/edu/finish", apiFinish());
-};
