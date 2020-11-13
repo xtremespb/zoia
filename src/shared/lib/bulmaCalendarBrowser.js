86e2b959126501d85cdcc0b2b21bdb7a86955253
Michael Matveev <xtreme@rh1.ru>
2020-11-13 14:21:27 +0300

    Fixed several FormBuilder-related bugs

--- src/shared/lib/bulmaCalendarBrowser.js
+++ src/shared/lib/bulmaCalendarBrowser.js
@@ -1,3 +1,3 @@
-const bulmaCalendar = require("bulma-extensions/bulma-calendar/build/js/bulma-calendar");
+const bulmaCalendar = require("bulma-extensions/bulma-calendar/dist/js/bulma-calendar"); // bulma-extensions/bulma-calendar/build/js/bulma-calendar
 
 module.exports = bulmaCalendar;
