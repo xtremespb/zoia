56fb64288b4490b235966eb0144bfb02c7b25758
SameOldSong <43583853+SameOldSong@users.noreply.github.com>
2020-12-06 20:57:09 +0100

    Update trigger_gitlab.py

--- .github/workflows/trigger_gitlab.py
+++ .github/workflows/trigger_gitlab.py
@@ -3,7 +3,7 @@ import json;
 import time;
 import sys, os;
 
-#trigger pipeline in GitLab
+# trigger pipeline in GitLab
 
 GITLAB_TOKEN = os.environ.get('GITLAB_TOKEN')
 TRIGGER_PIPELINE_URL = 'https://gitlab.com/api/v4/projects/19934840/trigger/pipeline?token=' + str(GITLAB_TOKEN) + '&ref=master'
@@ -12,12 +12,19 @@ trigger_response = requests.post(TRIGGER_PIPELINE_URL)
 print(trigger_response)
 pipeline_id = json.loads(trigger_response.text)['id']
 
-#wait for pipeline to finish
-status = "running";
+
+# prepare GET request to check pipeline status
 
 PIPELINE_STATUS_URL = 'https://gitlab.com/api/v4/projects/19934840/pipelines/' + str(pipeline_id)
 GITLAB_READ_TOKEN = os.environ.get('GITLAB_READ_TOKEN')
-headers = json.dumps({'content-type': 'application/json', 'Accept-Charset': 'UTF-8', 'PRIVATE-TOKEN':  GITLAB_READ_TOKEN})
+headers = {}
+headers['content-type'] = 'application/json'
+headers['PRIVATE-TOKEN'] = GITLAB_READ_TOKEN
+headers['Accept-Charset'] = 'UTF-8'
+
+# wait for pipeline to finish
+
+status = "running";
 while(status == "running" or status == "pending"):
   time.sleep(30)
   response = requests.get(url=PIPELINE_STATUS_URL, headers=headers)
