2d0c8c25e642f1e909acef129830196ec12a6362
SameOldSong <43583853+SameOldSong@users.noreply.github.com>
2021-01-30 21:34:08 +0100

    Update trigger_gitlab.py

--- .github/workflows/trigger_gitlab.py
+++ .github/workflows/trigger_gitlab.py
@@ -4,16 +4,16 @@ import time;
 import sys, os;
 
 # send GitHub commit to GitLab, thereby triggering pipeline
-
+    
 GITLAB_TOKEN = os.environ.get('GITLAB_TOKEN')
  
 GITLAB_BRANCH = os.environ.get('GITLAB_BRANCH')
 print("GitLab branch to send commit to = " + GITLAB_BRANCH)
 
-COMMIT_MESSAGE = os.environ.get('COMMIT_MESSAGE')
+COMMIT_MESSAGE = os.environ.get('github.event.head_commit.message')
 print("Commit message to send to GitLab: " + COMMIT_MESSAGE)
 
-GITHUB_BRANCH = os.environ.get('COMMIT_BRANCH')
+GITHUB_BRANCH = os.environ.get('GITHUB_HEAD_REF')
 print("Commit branch to be cloned in GitLab = " + GITHUB_BRANCH)
 
 
