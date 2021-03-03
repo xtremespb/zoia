7171b3aa1a594ef1fe3c670b392ec2c00884faa3
Michael Matveev <xtreme@rh1.ru>
2021-03-03 18:21:45 +0300

    fix: missing CLI and TEST builds

--- dev/config.js
+++ dev/config.js
@@ -19,8 +19,8 @@ module.exports = (env, argv) => {
     const moduleDirs = fs.readdirSync(path.resolve(`${__dirname}/../${argv.type === "update" ? "update" : "src"}/modules`)).filter(d => !d.match(/^\./));
     const configWebClient = require("./client")(moduleDirs, markoPlugin, argv);
     const configWebServer = require("./server")(markoPlugin, argv);
-    // const configTest = require("./test")(argv);
-    // const configCli = require("./cli")(argv);
+    const configTest = require("./test")(argv);
+    const configCli = require("./cli")(argv);
     utils.cleanUpWeb(argv);
     utils.ensureDirectories(config);
     utils.generateModulesConfig(moduleDirs, languages, argv);
@@ -31,7 +31,6 @@ module.exports = (env, argv) => {
     utils.copyMailTemplates(argv);
     utils.runBuildScripts(moduleDirs, argv);
     console.log("Starting Webpack...");
-    // webpackConfig.push(configWebClient, configWebServer, configTest, configCli);
-    webpackConfig.push(configWebClient, configWebServer);
+    webpackConfig.push(configWebClient, configWebServer, configTest, configCli);
     return webpackConfig;
 };
