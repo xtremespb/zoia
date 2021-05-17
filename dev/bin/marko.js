6eff5b76c570827b49c666dadb72dfafa133d27b
Michael Matveev <xtreme@rh1.ru>
2021-05-17 17:01:23 +0300

    Feature: linter for Marko files

--- dev/bin/marko.js
+++ dev/bin/marko.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const fs = require("fs-extra");
 const path = require("path");
 const {
@@ -14,6 +15,22 @@ const processMarkoFiles = async dir => {
         } else if (path.extname(filePath) === ".marko") {
             console.log(filePath);
             prettyPrintFile(filePath, {});
+            const fileData = (await fs.readFile(filePath, "utf8")).replace(/\\\$\{/gm, "${").replace(/\\\$!\{/gm, "$!{").split(/\n|\r\n/);
+            let tagFound = false;
+            let fileDataNew = "";
+            for (const line of fileData) {
+                if (!tagFound && line.match(/^</)) {
+                    tagFound = true;
+                }
+                if (tagFound && line.match(/^\n|\r\n$/)) {
+                    // Do something, we're not adding an empty line
+                } else {
+                    // Add a new line, it's not empty
+                    fileDataNew += `${line}\n`;
+                }
+            }
+            fileDataNew = tagFound ? `${fileDataNew.trim()}\n` : `${fileData.join("\n")}\n`;
+            await fs.writeFile(filePath, fileDataNew, "utf8");
         }
     }
 };
