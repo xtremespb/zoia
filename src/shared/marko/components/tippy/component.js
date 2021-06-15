508f11223776a170a0e1426799bbd35de300e0dc
Michael Matveev <xtreme@rh1.ru>
2021-06-15 14:41:41 +0300

    Fix: updated NPM modules Fix: tippy-related fixes for mobile devices

--- src/shared/marko/components/tippy/component.js
+++ src/shared/marko/components/tippy/component.js
@@ -1,22 +1,36 @@
 const tippy = require("tippy.js").default;
 
 module.exports = class {
-    onMount() {
-        this.tippyInstances = tippy("[data-tippy-content]", {
-            hideOnClick: true,
+    initTippy() {
+        this.tippyInstances = [];
+        this.tippyElements = tippy("[data-tippy-content]", {
+            onCreate: instance => {
+                this.tippyInstances.push(instance);
+            }
         });
+    }
+
+    onMount() {
+        tippy.setDefaultProps({
+            arrow: true,
+            zIndex: 30,
+        });
+        setTimeout(() => this.initTippy(), 100);
         window.__zoiaTippyJs = {
             reset: this.reset.bind(this),
-            hide: this.hide.bind(this)
+            hide: this.hide.bind(this),
         };
     }
 
     reset() {
         this.tippyInstances.map(i => i.destroy());
-        setTimeout(() => this.tippyInstances = tippy("[data-tippy-content]"), 100);
+        setTimeout(() => this.initTippy(), 100);
     }
 
     hide() {
-        Array.from(document.querySelectorAll("[data-tippy-root]")).map(i => i.style.visibility = "hidden");
+        Array.from(document.querySelectorAll("[data-tippy-root]")).map(i => {
+            i.style.visibility = "hidden";
+            i.style.zIndex = 30;
+        });
     }
 };
