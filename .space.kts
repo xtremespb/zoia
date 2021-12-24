5bc169631363e3f9ae2c3a8d3f150efd6723b03d
Tata <tatiana.nikolaeva@gmx.net>
2021-12-24 19:48:51 +0100

    Removing obsolete TeamSpace config file

--- .space.kts
+++
@@ -1,9 +0,0 @@
-/**
-* JetBrains Space Automation
-* This Kotlin-script file lets you automate build activities
-* For more info, see https://www.jetbrains.com/help/space/automation.html
-*/
-
-job("Hello World!") {
-    container(displayName = "Say Hello", image = "hello-world")
-}
